50ccad937d632eaf9f9bdf1cae27ddb922f9728e
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2024-06-24 13:58:36 -0400

    change source url for test-slz

--- test-slz/main.tf
+++ test-slz/main.tf
@@ -4,7 +4,7 @@
 
 module "landing_zone" {
   #source           = "https://cm.globalcatalog.cloud.ibm.com/api/v1-beta/offering/source/archive//patterns/vsi?archive=tgz&flavor=standard&installType=fullstack&kind=terraform&name=deploy-arch-ibm-slz-vsi&version=v5.3.1"
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v4.0.1"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns/vsi.git?ref=v5.24.7"
   prefix           = var.prefix
   region           = "us-south"
   ibmcloud_api_key = var.ibmcloud_api_key
